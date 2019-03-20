9731ed4fda3e5e94f035832b0bf4c70d454e2096
Serena Berry <siberry226@gmail.com>
2019-03-20 18:09:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT temperment, COUNT(temperment) FROM bears GROUP BY temperment"
 end
 
 def selects_bear_that_killed_Tim
